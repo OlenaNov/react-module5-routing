d058ddcfebe17ae6b09ddc881ee0a6132dd7f792
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-04-23 19:07:10 +0200

    start

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/react-module5-routing">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
